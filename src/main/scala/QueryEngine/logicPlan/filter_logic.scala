76efbe5ac1ceb97bbe95fe2df2defec1a4d1d440
Roman <378753028@qq.com>
2019-05-09 10:49:15 +0800

    filter_logic update

--- src/main/scala/QueryEngine/logicPlan/filter_logic.scala
+++ src/main/scala/QueryEngine/logicPlan/filter_logic.scala
@@ -9,7 +9,7 @@ import common.Catalog
 case class filter_logic(val columnName: String, val compare: String, val columnValue: String, child: logicPlanTree) extends logicPlanTree {
   lazy val column: Column = Catalog.getTableColumn(tableName, columnName)
   var tableName: String = null
-
+  val columnNam1e: String = null
   def filterColumn(): Int = {
     Catalog.getCoulumnIndex(tableName, column)
   }
